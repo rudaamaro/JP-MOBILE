d72ca1d978afae119a0d86f5f53ce679a788a1cf
rudaamaro <contato.amaror@gmail.com>
2025-08-14 17:20:58 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,12 +3,11 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebas
 import { getAuth }       from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 import { getFirestore }  from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
-// Use exatamente os valores que o Firebase Console mostrou para o SEU app Web
 export const firebaseConfig = {
   apiKey: "AIzaSyA89u1JP_rg6sWYWWobFB5Bg5JFaho1u7M",
   authDomain: "flashcards-jp-pt.firebaseapp.com",
   projectId: "flashcards-jp-pt",
-  // ⚠️ Corrigido: o bucket de config é *.appspot.com (não *.firebasestorage.app)
+  // O 'storageBucket' deve ser o NOME do bucket (.appspot.com), não o domínio .firebasestorage.app
   storageBucket: "flashcards-jp-pt.appspot.com",
   messagingSenderId: "163392706044",
   appId: "1:163392706044:web:1f6f0ed3a65b367c534e79"
